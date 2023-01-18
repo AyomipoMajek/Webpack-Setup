017ba067e7ccabf8cc47aba6506178bea10271f0
Ayomipo Majek <ayomipomajek@gmail.com>
2023-01-18 18:26:15 +0100

    todo list worked on

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { template } = require('lodash');
+// const { template } = require('lodash');
 
 module.exports = {
+  type: "module",
   mode: 'development',
   entry: {
-   bundle : path.resolve(__dirname, "./src/index.js")
+    bundle: path.resolve(__dirname, './src/index.js'),
   },
   devServer: {
     static: './dist',
   },
- plugins: [
-   new HtmlWebpackPlugin({
-     title: 'Output Management',
-     template: "./src/template.html"
-   }),
- ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/template.html',
+    }),
+  ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
   optimization: {
     runtimeChunk: 'single',
@@ -27,9 +28,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test:/\.css$/,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
-      }
-    ]
+      },
+    ],
   },
 };
